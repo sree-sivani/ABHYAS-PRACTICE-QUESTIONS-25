5d1758edf6ac00be5c241f1da339d694b34efb2e
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2024-12-14 11:31:39 +0530

    Update program10.py

--- program10.py
+++ program10.py
@@ -1,1 +1,15 @@
+def sumOfDigits(N):
+    
 
+    sum = 0
+    while N > 0:
+        digit = N % 10
+        sum += digit
+        N //= 10
+
+    return sum
+    
+n = int(input())
+res = sumOfDigits(n)
+
+print(res)
