9ae8aa2a751145ed4bc4afc8b56421f56db35914
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2024-12-14 11:26:48 +0530

    Update program9.py

--- program9.py
+++ program9.py
@@ -1,1 +1,18 @@
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 
+year = int(input())
+
+if is_leap_year(year):
+    print("LEAP YEAR")
+else:
+    print("NOT LEAP YEAR")
