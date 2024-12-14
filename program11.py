c5ea727594cf5d43435d84d411c7b08d6c0fc9e0
Sree Sivani <111432178+sree-sivani@users.noreply.github.com>
2024-12-14 11:32:05 +0530

    Update program11.py

--- program11.py
+++ program11.py
@@ -1,1 +1,27 @@
 
+def isSumPalindrome(n):
+    
+
+    for _ in range(5):
+        rev = 0
+        temp = n
+        while temp > 0:
+            digit = temp % 10
+            rev = rev * 10 + digit
+            temp //= 10
+
+        if n == rev:
+            return n
+
+        n += rev
+
+    return -1
+    
+    
+n = (input())
+
+arr = n.split()
+
+res = isSumPalindrome(int(arr[-1]))
+
+print(res)
